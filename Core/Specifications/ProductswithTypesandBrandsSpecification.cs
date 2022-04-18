2dad45d69cc4677caea431896856aadc87824bdb
priyankarpatil <patilpriyankar23@gmail.com>
2022-04-18 14:45:15 +0530

    Paging,Filtering,Searching,Sorting

--- Core/Specifications/ProductswithTypesandBrandsSpecification.cs
+++ Core/Specifications/ProductswithTypesandBrandsSpecification.cs
@@ -9,10 +9,38 @@ namespace Core.Specifications
 {
     public class ProductswithTypesandBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductswithTypesandBrandsSpecification()
+        public ProductswithTypesandBrandsSpecification(ProductSpecParams productParams)
+             : base(x =>
+             (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower()
+             .Contains(productParams.Search)) && 
+             (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+             (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+             )
+
+       
         {
+
            AddInclude(x => x.ProductType);
            AddInclude(x => x.ProductBrand);
+           AddOrderBy(x => x.Name);
+           ApplyPaging(productParams.PageSize * (productParams.PageIndex -1),productParams.PageSize);
+
+           if(!string.IsNullOrEmpty(productParams.Sort))
+           {
+               switch(productParams.Sort)
+               {
+                   case "PriceAsc" :
+                     AddOrderBy(p => p.Price);
+                     break;
+                   case "PriceDesc" :
+                      AddOrderByDescending(p => p.Price);
+                      break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+               }
+
+           }
 
         }
 
